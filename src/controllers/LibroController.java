313b23ede058441fb17ec87a62b25459fe28b445
xJoffrex <mguaricelac@gmail.com>
2025-07-16 16:37:13 -0500

    Test 2

--- src/controllers/LibroController.java
+++ src/controllers/LibroController.java
@@ -5,14 +5,17 @@ import java.util.*;
 
 public class LibroController {
     public Set<Book> procesarLibros(List<Book> libros) {
-        Set<Book> resultado = new TreeSet<>((b1, b2) -> {
-            int cmpTitulo = b2.getTitulo().compareTo(b1.getTitulo()); 
-            if (cmpTitulo != 0) return cmpTitulo;
+        Comparator<Book> comparador = new Comparator<Book>() {
+            @Override
+            public int compare(Book b1, Book b2) {
+                int compaTitulo = b2.getTitulo().compareTo(b1.getTitulo());
+                if (compaTitulo != 0) return compaTitulo;
 
-            int cmpAnio = Integer.compare(b1.getAnio(), b2.getAnio()); 
-            return cmpAnio;
-        });
+                return Integer.compare(b1.getAnio(), b2.getAnio());
+            }
+        };
 
+        Set<Book> resultado = new TreeSet<>(comparador);
         resultado.addAll(libros);
         return resultado;
     }
